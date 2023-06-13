583da7c7c06b9b18013859662328f19d46726875
ferriyusra <ferisakuragi@gmail.com>
2023-06-13 10:12:18 +0700

    create table, model and add aggregate function

--- database.sql
+++ database.sql
@@ -13,3 +13,26 @@ create table customers (
   constraint customers_email_unique unique (email),
   constraint customers_phone_unique unique (phone)
 )engine innodb;
+
+create table products (
+  id varchar(100) not null,
+  name varchar(100) not null,
+  price int not null,
+  stock int not null,
+  category varchar(100) not null,
+  primary key (id)
+)engine innodb;
+
+insert into products (id, name, price, stock, category)
+values ("P0001", "A", 1000, 100, "K1"),
+("P0002", "B", 2000, 200, "K1"),
+("P0003", "C", 3000, 300, "K1"),
+("P0004", "D", 4000, 400, "K1"),
+("P0005", "E", 5000, 500, "K1");
+
+insert into products (id, name, price, stock, category)
+values ("P0006", "A", 1000, 100, "K2"),
+("P0007", "B", 2000, 200, "K2"),
+("P0008", "C", 3000, 300, "K2"),
+("P0009", "D", 4000, 400, "K2"),
+("P00010", "E", 5000, 500, "K2");
